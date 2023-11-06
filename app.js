311520881fc077ad712f53a41d0f0598017d93c8
Prnzdiamond <oseahumenagboifoh2gmail.com>
2023-11-06 09:53:56 -0700

    new

---
+++ app.js
@@ -0,0 +1,9 @@
+var hm = document.getElementById("hm-mn");
+var mn= document.getElementById("mn-items");
+
+function dropdown () {
+    hm.classList.toggle("hm-mn-cl");
+    mn.classList.toggle("ulrep");
+}
+
+// function for type writing effect
